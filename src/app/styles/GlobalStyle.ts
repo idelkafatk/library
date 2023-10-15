60969d508cd02c212f45a38ee931459a3f5d7989
Idel Fatkhullin <maximbravin@gmail.com>
2023-10-16 02:07:33 +0600

    [LIBRARY-4] added meta resize content

--- src/app/styles/GlobalStyle.ts
+++ src/app/styles/GlobalStyle.ts
@@ -1,13 +1,6 @@
 import { createGlobalStyle } from 'styled-components/macro'
 
 const GlobalStyle = createGlobalStyle`
-  .content {
-    padding-top: constant(safe-area-inset-top); /* Safari */
-    padding-top: env(safe-area-inset-top); /* iOS 11.2+ */
-    padding-bottom: constant(safe-area-inset-bottom); /* Safari */
-    padding-bottom: env(safe-area-inset-bottom); /* iOS 11.2+ */
-  }
-
   body {
     margin: 0;
     padding: 0;
