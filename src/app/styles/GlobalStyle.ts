13d9a8b49b8e8d58a3ec35388af571c7eea92004
Idel Fatkhullin <maximbravin@gmail.com>
2023-10-16 02:01:03 +0600

    [LIBRARY-4] added safe area

--- src/app/styles/GlobalStyle.ts
+++ src/app/styles/GlobalStyle.ts
@@ -1,68 +1,13 @@
-import { css } from 'styled-components'
 import { createGlobalStyle } from 'styled-components/macro'
 
-const burgerTransitionstyle = css`
-  .icon-transition-enter {
-    transform: rotate(0deg);
-    transition: transform 0.3s;
-  }
-
-  .icon-transition-enter-active {
-    transform: rotate(45deg);
-    transition: transform 0.3s;
-  }
-
-  .icon-transition-exit {
-    transform: rotate(45deg);
-    transition: transform 0.3s;
-  }
-
-  .icon-transition-exit-active {
-    transform: rotate(0deg);
-    transition: transform 0.3s;
-  }
-
-  .icon-transition-enter,
-  .icon-transition-exit {
-    display: block;
-  }
-
-  .icon-transition-exit-done {
-    display: none;
-  }
-
-  .modal-transition-enter {
-    opacity: 0;
-    transition: opacity 0.3s;
-  }
-
-  .modal-transition-enter-active {
-    opacity: 1;
-    transition: opacity 0.3s;
-  }
-
-  .modal-transition-exit {
-    opacity: 1;
-    transition: opacity 0.3s;
-  }
-
-  .modal-transition-exit-active {
-    opacity: 0;
-    transition: opacity 0.3s;
-  }
-
-  .modal-transition-enter,
-  .modal-transition-exit {
-    display: block;
-  }
-
-  .modal-transition-exit-done {
-    display: none;
-  }
-`
-
 const GlobalStyle = createGlobalStyle`
-  ${burgerTransitionstyle}
+  .content {
+    padding-top: constant(safe-area-inset-top); /* Safari */
+    padding-top: env(safe-area-inset-top); /* iOS 11.2+ */
+    padding-bottom: constant(safe-area-inset-bottom); /* Safari */
+    padding-bottom: env(safe-area-inset-bottom); /* iOS 11.2+ */
+  }
+
   body {
     margin: 0;
     padding: 0;
